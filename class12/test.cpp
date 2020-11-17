73a24132037a0b82b09f70237278c9b8255bc4dc
mosina916 <mosinaashraf1913@gmail.com>
2020-11-17 14:18:57 +0530

    study abacus

---
+++ class12/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    cout <<s<<endl;
+    // char a[11]={'a','e','i','o','u','A','E','I','O','U','\0'};
+    // int j=0;
+    // while(s[j]!='\0'){
+    //     char x=s[j];
+    //     for(int i=0;i<11;i++){
+
+        
+    //     }
+    // }
+    return 0;
+}
