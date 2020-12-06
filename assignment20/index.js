5e2ef45bb75a66796b649fef6835b7884ceb8589
mosina916 <mosinaashraf1913@gmail.com>
2020-12-06 12:12:04 +0530

    study abacus

---
+++ assignment20/index.js
@@ -0,0 +1,6 @@
+var fname=prompt("What is your first name?");
+var lname=prompt("What is your Last name?");
+var age=prompt("What is your Age?");
+
+console.log("Your full name is " + fname + " "+lname);
+console.log("You are "+ age +" years old");
